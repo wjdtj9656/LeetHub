4dd0c84e9fbfa15587ee37e019161aaa13fc6a8f
wjdtj9656 <wjdtj9656@naver.com>
2022-11-07 21:31:55 +0900

    Time: 85 ms (74.94%), Space: 42.4 MB (7.00%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -4,7 +4,6 @@
  */
 var maximum69Number  = function(num) {
     let numStr = String(num).split('');
-    console.log(numStr);
     let index = numStr.indexOf('6');
     if(index === -1) return num;
     else{
